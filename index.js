887dfd378d2a6c3c43810ee9464d6fb187218bb7
harshjha872 <harsh.1923ec1128@kiet.edu>
2021-06-27 00:38:37 +0530

    first

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Node js');
+const fs = require('fs');
+// const data = fs.readFileSync('./txt/read.txt', 'utf-8');
+const data2 = fs.readFile('./txt/read.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
+console.log('First statement');
