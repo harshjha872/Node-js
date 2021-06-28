0d6051f79fa7ccc295eb18568ecdbe5200fa2a05
harshjha872 <harsh.1923ec1128@kiet.edu>
2021-06-28 08:54:33 +0530

    createServer

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
 // const data = fs.readFileSync('./txt/read.txt', 'utf-8');
-const data2 = fs.readFile('./txt/read.txt', 'utf-8', (err, data) => {
-  console.log(data);
+// const data2 = fs.readFile('./txt/read.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+// console.log('First statement');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.end('Hello to the node js createdserver');
 });
-console.log('First statement');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('server running');
+});
