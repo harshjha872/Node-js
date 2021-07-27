9606b9f1f6db7b95bece34f63a519998757e5a99
harshjha872 <harsh.1923ec1128@kiet.edu>
2021-07-27 11:10:41 +0530

    pagination

--- Shop App/routes/public.js
+++ Shop App/routes/public.js
@@ -5,6 +5,8 @@ const products = require('../modals/product');
 const Pubrouter = express.Router();
 const Product = require('../Database/database-mongoose');
 
+let NoOfProductsDisplayed = 3;
+
 Pubrouter.get('/', (req, res, next) => {
   // products.fetchAll((prod) => {
   //   res.render('public', {
@@ -14,17 +16,29 @@ Pubrouter.get('/', (req, res, next) => {
   //     ListOfproducts: prod,
   //   });
   // });
+  let previous = false;
+  let nextPage = true;
+  const page = +req.query.page;
+
+  if (page === 1) previous = false;
+  if (page === NoOfProductsDisplayed) nextPage = false;
 
   //mongoose function find() to fetch data from database
-  Product.find().then((products) => {
-    res.render('public', {
-      title: 'home page',
-      activeClass: 'active',
-      route: '/',
-      ListOfproducts: products,
-      isloggedIn: req.session.loggedIn,
+  Product.find()
+    .skip((page - 1) * NoOfProductsDisplayed)
+    .limit(NoOfProductsDisplayed)
+    .then((products) => {
+      res.render('public', {
+        title: 'home page',
+        activeClass: 'active',
+        route: '/',
+        ListOfproducts: products,
+        isloggedIn: req.session.loggedIn,
+        previous: previous,
+        next: nextPage,
+        totalProduct: NoOfProductsDisplayed,
+      });
     });
-  });
 });
 
 module.exports = Pubrouter;
